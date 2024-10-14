08bfcaa841fda4f88ec74d09f9f8e568e5e8bd78
nguyenanhcd <anh487303@gmail.com>
2024-10-14 20:40:38 +0700

    know about nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,12 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  // This method is a more concise version of the below method and this way can only used in ts
+
+  // private readonly appService: AppService;
+  // constructor(appService: AppService) {
+  //   this.appService = appService;
+  // }
 
   @Get()
   getHello(): string {
