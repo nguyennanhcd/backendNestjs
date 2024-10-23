44feaa60eb4dfb9c79a14ba87a64790410404ea7
nguyenanhcd <anh487303@gmail.com>
2024-10-23 13:07:38 +0700

    done adding plugin

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,23 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-
   constructor(
     @InjectModel(User.name)
-    private userModel: Model<User>
-  ) { }
+    private userModel: SoftDeleteModel<UserDocument>,
+  ) {}
 
   getHashPassword = (password: string) => {
     const salt = genSaltSync(10);
     const hash = hashSync(password, salt);
     return hash;
-  }
+  };
 
   async create(hoidanit: CreateUserDto) {
     const hashPassword = this.getHashPassword(hoidanit.password);
@@ -26,8 +26,8 @@ export class UsersService {
     let user = await this.userModel.create({
       email: hoidanit.email,
       password: hashPassword,
-      name: hoidanit.name
-    })
+      name: hoidanit.name,
+    });
     return user;
   }
 
@@ -36,35 +36,35 @@ export class UsersService {
   }
 
   findOne(id: string) {
-    if (!mongoose.Types.ObjectId.isValid(id))
-      return `not found user`;
+    if (!mongoose.Types.ObjectId.isValid(id)) return `not found user`;
 
     return this.userModel.findOne({
-      _id: id
-    })
+      _id: id,
+    });
   }
 
   findOneByUsername(username: string) {
     return this.userModel.findOne({
-      email: username
-    })
+      email: username,
+    });
   }
-
 
   isValidPassword(password: string, hash: string) {
     return compareSync(password, hash);
   }
 
   async update(updateUserDto: UpdateUserDto) {
-    return await this.userModel.updateOne({ _id: updateUserDto._id }, { ...updateUserDto })
+    return await this.userModel.updateOne(
+      { _id: updateUserDto._id },
+      { ...updateUserDto },
+    );
   }
 
   remove(id: string) {
-    if (!mongoose.Types.ObjectId.isValid(id))
-      return `not found user`;
+    if (!mongoose.Types.ObjectId.isValid(id)) return `not found user`;
 
-    return this.userModel.deleteOne({
-      _id: id
-    })
+    return this.userModel.softDelete({
+      _id: id,
+    });
   }
 }
