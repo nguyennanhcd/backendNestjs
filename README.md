8f3bceff0370d08398dd6d11f75bbb0e9f6b10a8
nguyenanhcd <anh487303@gmail.com>
2024-10-23 20:55:12 +0700

    done company pagination

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 When you work with SQL database, the db folder's name is entities
 When you work with mongodb database, the db folder's name is schema
+
+mongoose hook
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schema/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/user.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class CompaniesService {
@@ -24,8 +25,33 @@ export class CompaniesService {
     return company;
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    // Type casting inside a destructuring object.
+    const { filter, sort, population } = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+    const result = await this.companyModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage,
+        pageSize: limit,
+        pages: totalPages,
+        total: totalItems,
+      },
+      result, //kết quả query
+    };
   }
 
   findOne(id: number) {
