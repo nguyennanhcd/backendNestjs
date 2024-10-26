12b1af23110ec7f4a44e8f2cfd737283c26e5def
nguyenanhcd <anh487303@gmail.com>
2024-10-26 08:38:25 +0700

    update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,67 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
 
-//data transfer object // class = { }
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
 export class CreateUserDto {
-    @IsEmail({}, { message: 'Email không đúng định dạng', })
-    @IsNotEmpty({ message: 'Email không được để trống', })
-    email: string;
+  @IsNotEmpty({ message: 'Name must not be empty ' })
+  name: string;
 
-    @IsNotEmpty({ message: 'Password không được để trống', })
-    password: string;
+  @IsEmail({}, { message: 'Email is invalid' })
+  @IsNotEmpty({ message: 'Email must not be empty' })
+  email: string;
 
-    name: string;
-    address: string;
+  @IsNotEmpty({ message: 'Password must not be empty' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Age must not be empty' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender must not be empty' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address must not be empty' })
+  address: string;
+
+  @IsNotEmpty({ message: 'Role must not be empty' })
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name must not be empty ' })
+  name: string;
+
+  @IsEmail({}, { message: 'Email is invalid' })
+  @IsNotEmpty({ message: 'Email must not be empty' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password must not be empty' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Age must not be empty' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender must not be empty' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address must not be empty' })
+  address: string;
 }
